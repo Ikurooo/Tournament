c2ab07197d4e0a405dbbd0bb749e400a8e70c543
Ivan <ivanecankov@gmail.com>
2024-03-06 20:42:55 +0100

    edit page woohoo

---
+++ frontend/src/app/component/horse/edit-horse/edit-horse.component.ts
@@ -0,0 +1,14 @@
+// edit-horse.component.ts
+
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-horse',
+  templateUrl: './edit-horse.component.html',
+  standalone: true,
+  styleUrls: ['./edit-horse.component.css']
+})
+export class EditHorseComponent {
+  // Add component logic as needed
+}
