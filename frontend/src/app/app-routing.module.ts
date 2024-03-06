7cb9cc10d44e61a3fb3c0ee4d4fce7e1a8585f20
Ivan <ivanecankov@gmail.com>
2024-03-06 20:33:24 +0100

    horse edit routing file update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HorseCreateEditComponent, HorseCreateEditMode} from './component/horse/horse-create-edit/horse-create-edit.component';
-import {HorseComponent} from './component/horse/horse.component';
-import {TournamentCreateComponent} from "./component/tournament/tournament-create/tournament-create.component";
-import {TournamentStandingsComponent} from "./component/tournament/tournament-standings/tournament-standings.component";
+// app-routing.module.ts
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HorseCreateEditComponent, HorseCreateEditMode } from './component/horse/horse-create-edit/horse-create-edit.component';
+import { HorseComponent } from './component/horse/horse.component';
+import { TournamentCreateComponent } from "./component/tournament/tournament-create/tournament-create.component";
+import { TournamentStandingsComponent } from "./component/tournament/tournament-standings/tournament-standings.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'horses', pathMatch: 'full'},
-  {path: 'horses', children: [
-    {path: '', component: HorseComponent},
-    {path: 'create', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.create}},
-  ]},
-  {path: 'tournaments', children: [
-    {path: 'create', component: TournamentCreateComponent},
-    {path: 'standings/:id', component: TournamentStandingsComponent}
-  ]},
-  {path: '**', redirectTo: 'horses'},
+  { path: '', redirectTo: 'horses', pathMatch: 'full' },
+  {
+    path: 'horses', children: [
+      { path: '', component: HorseComponent },
+      { path: 'create', component: HorseCreateEditComponent, data: { mode: HorseCreateEditMode.create } },
+      { path: 'edit/:id', component: HorseCreateEditComponent, data: { mode: HorseCreateEditMode.edit } }, // Add this line for editing a horse
+    ]
+  },
+  {
+    path: 'tournaments', children: [
+      { path: 'create', component: TournamentCreateComponent },
+      { path: 'standings/:id', component: TournamentStandingsComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'horses' },
 ];
 
 @NgModule({
